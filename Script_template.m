6ff85cf26387a17791932c597037bbaa537403ee
wst-017debruyne <sitanwen017@gmail.com>
2026-04-24 13:26:38 +0800

    Add files via upload

--- Script_template.m
+++ Script_template.m
@@ -18,7 +18,7 @@
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 disp('Task 1')
 disp('(b)')
-duration = 600;                                                             %记得改成600s!!!!!!!!
+duration = 120;                                                             %记得改成600s!!!!!!!!
 
 all_temper = [];
 record = zeros(2, 0);                                                       %这里是记录的集合变量
@@ -29,12 +29,13 @@ for n = 1:duration
     disp(temperature)
     judgement = floor(n/60);
     if (n/60)-judgement ==0                                                 %写到这里了 继续改记得
-
+        temp= [judgement, temperature];
+        record= [record; temp];
 
     end
     pause(1);
 end
-            
+
 max_temp = max(all_temper); 
 min_temp = min(all_temper);
 aver_temp = mean(all_temper);
@@ -44,12 +45,46 @@ fprintf('The average temperature: %.4f.\n', aver_temp);
 
 
 disp('c')
-x= linspace(0,599,600);
+disp('The graph has been plotted.')
+x= linspace(0,duration-1,duration);                                                       %注意这里随着时间改变也要改!lo
 plot(x, all_temper)
 xlabel('Time/s')
 ylabel('Temperature ^\circ C');
 
+disp('(d)')
+t=datetime('now','Format','MM/dd/uuuu');
+location= input('Please input the location Name: ','s');
+fprintf('\n');
 
+text = fopen('capsule_temperature.txt');                                    %open the txt file
+
+fprintf2(text,'Data logging initiated - %s \n',t);                          %Print the instant result at the specific minute
+fprintf2(text,'Location- %s\n',location);
+fprintf2(text,'\n');
+
+width = 15;
+for dat = 1:size(record,1)
+    time = record(dat, 1);
+    spe_temper = record(dat, 2);
+
+    fprintf2(text,'%-*s %.2f\n', width, 'minute:', time);
+    fprintf2(text,'%-*s %.2f C\n', width, 'Temperature:', spe_temper);
+    fprintf2(text,'\n')
+end
+
+fprintf2(text,'%-*s %.2f C\n', width, 'Max temp ', max_temp);
+fprintf2(text,'%-*s %.2f C\n', width, 'Min temp ', min_temp);
+fprintf2(text,'%-*s %.2f C\n', width, 'Average temp ', aver_temp);
+
+fprintf2('Data logging terminated')
+
+fclose(text);                                                               %close the txt file
+
+function fprintf2(text, varargin)
+     fprintf(varargin{:});
+     fprintf(text, varargin{:});
+
+end
 
 
 
